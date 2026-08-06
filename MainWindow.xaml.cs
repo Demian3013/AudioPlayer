abaad100c247c00fffc803052b3d1f6d3b97f9f7
Demian <roy.rudem@mail.ru>
2026-08-06 09:59:15 +0300

    Добавил - Таймер обновления позиции - Перемотка с захватом - Громкость и скорость

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,8 +5,10 @@ using System.IO;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Threading;
 using TagLib;
 
 namespace AudioPlayer;
@@ -18,8 +20,10 @@ public partial class MainWindow
         InitializeComponent();
 
         FolderTreeManager.BuildTree(FoldersTreeView);
-        
+
         DataContext = this;
+
+        this.Loaded += MainWindow_Loaded;
     }
 
     private static readonly Regex SoundFileRegex = MyRegex();
@@ -31,8 +35,15 @@ public partial class MainWindow
 
     public ObservableCollection<AudioFileInfo> AudioFiles { get; set; } = [];
 
+    private AudioFileInfo? _currentTrack = null;
+
     private MediaPlayer Player { get; set; } = new();
-    
+
+    private bool _isPlaying = false;
+    private bool _isDragging = false;
+    private TimeSpan _totalDuration;
+    private DispatcherTimer _updateTimer;
+
     private void FoldersTreeView_OnSelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
     {
         if (FoldersTreeView.SelectedItem is not TreeViewItem item) return;
@@ -81,9 +92,9 @@ public partial class MainWindow
                 AudioFiles.Add(new AudioFileInfo
                 {
                     FilePath = file,
-                    Name     = Path.GetFileNameWithoutExtension(file),
-                    Artist   = "Ошибка",
-                    Album    = "Ошибка",
+                    Name = Path.GetFileNameWithoutExtension(file),
+                    Artist = "Ошибка",
+                    Album = "Ошибка",
                     Duration = "00:00"
                 });
             }
@@ -92,7 +103,7 @@ public partial class MainWindow
 
     private void RewindButton_Click(object sender, RoutedEventArgs e)
     {
-        
+
     }
 
     private void PlayPauseButton_Click(object sender, RoutedEventArgs e)
@@ -101,90 +112,190 @@ public partial class MainWindow
         {
             MessageBox.Show("Не выбран трек или выбран медиафайл без аудио!", "Ошибка!", MessageBoxButton.OK,
                 MessageBoxImage.Warning);
+            return;
         }
 
-        // в начале трека, то есть не начали ещё проигрывание
-        if (Player.Position == TimeSpan.Zero)
+        if (_isPlaying)
         {
-            Player.Play();
+            Player.Pause();
+            PlayPauseImage.Source = new BitmapImage(new Uri("Image/stop.png", UriKind.Relative));
+            _isPlaying = false;
         }
         else
         {
-            Player.Pause();
+            Player.Play();
+            PlayPauseImage.Source = new BitmapImage(new Uri("Image/play.png", UriKind.Relative));
+            _isPlaying = true;
         }
     }
 
     private void ForwardButton_Click(object sender, RoutedEventArgs e)
     {
-        
+
     }
 
-    private void SpeedButton_Click(object sender, RoutedEventArgs e) 
-    { 
+    private void SpeedButton_Click(object sender, RoutedEventArgs e)
+    {
+        Button? button = sender as Button;
+        if (button == null) return;
 
+        string content = button.Content.ToString();
+
+        if (double.TryParse(content, out double speed))
+        {
+            Player.SpeedRatio = speed;
+        }
     }
 
     private void TrackDataGrid_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
     {
         if (TrackDataGrid.SelectedItem is not AudioFileInfo selectedTrackInfo) return;
-        
-        using var tagFile = TagLib.File.Create(selectedTrackInfo.FilePath);
 
-        #region TackInfoExtraction
-
-            if (tagFile.Tag.Pictures.Length != 0)
-            {
-                var picture = tagFile.Tag.Pictures[0];
-                var bitmapImageFromPicture = GetBitmapImageFromPicture(picture);
-                if (bitmapImageFromPicture != null)
-                {
-                    TrackImage.Source = bitmapImageFromPicture;
-                }
-            }
-            else
-            {
-                TrackImage.Source = new BitmapImage(new Uri(TrackPlaceholderPath, UriKind.Relative));
-            }
-            
-            TrackNameTextBlock.Text = selectedTrackInfo.Name;
-
-            TrackArtistTextBlock.Text = string.IsNullOrEmpty(tagFile.Tag.FirstPerformer)
-                ? "Неизвестен"
-                : tagFile.Tag.FirstPerformer;
-
-            TrackYearTextBlock.Text = tagFile.Tag.Year > 0
-                ? tagFile.Tag.Year.ToString()
-                : "Неизвестно";
-
-            TrackGenreTextBlock.Text = tagFile.Tag.Genres.Length > 0
-                ? string.Join(", ", tagFile.Tag.Genres)
-                : "Неизвестен";
-
-            TrackDistributionTextBlock.Text = string.IsNullOrEmpty(tagFile.Tag.Comment)
-                ? "Нет описания"
-                : tagFile.Tag.Comment;
-
-        #endregion
-
-        var selectedTackUri = new Uri(selectedTrackInfo.FilePath, UriKind.Absolute);
-        
-        Player.Pause();
-        ProgressSlider.Value = 0;
-        Player.Close();
-        Player.Open(selectedTackUri);
+        PlayTrack(selectedTrackInfo);
     }
+
+    private void OnMediaOpened(object sender, EventArgs e)
+    {
+        _totalDuration = Player.NaturalDuration.TimeSpan;
+        ProgressSlider.Value = 0;
+        StartUpdateTimer();
+        Player.Play();
+        _isPlaying = true;
+
+    }
+
 
     private static BitmapImage? GetBitmapImageFromPicture(IPicture p)
     {
         if (p.Data.IsEmpty) return null;
-        
+
         using var stream = new MemoryStream(p.Data.Data);
         var bmp = new BitmapImage();
         bmp.BeginInit();
         bmp.CacheOption = BitmapCacheOption.OnLoad;
         bmp.StreamSource = stream;
         bmp.EndInit();
-        
+
         return bmp;
+    }
+
+    private void ProgressSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+    {
+
+    }
+
+    private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+        ProgressSlider.ApplyTemplate();
+
+        var track = ProgressSlider.Template.FindName("PART_Track", ProgressSlider) as Track;
+        var thumb = track?.Thumb;
+        if (thumb != null)
+        {
+            thumb.DragStarted += Thumb_DragStarted;
+            thumb.DragCompleted += Thumb_DragCompleted;
+        }
+        Player.Volume = VolumeSlider.Value;
+    }
+
+    private void Thumb_DragStarted(object sender, DragStartedEventArgs e)
+    {
+        _isDragging = true;
+    }
+
+    private void Thumb_DragCompleted(object sender, DragCompletedEventArgs e)
+    {
+        _isDragging = false;
+
+        if (Player.NaturalDuration.HasTimeSpan)
+        {
+            double newPositionSeconds = _totalDuration.TotalSeconds * ProgressSlider.Value;
+            Player.Position = TimeSpan.FromSeconds(newPositionSeconds);
+        }
+    }
+    private void StartUpdateTimer()
+    {
+        _updateTimer = new DispatcherTimer();
+        _updateTimer.Interval = TimeSpan.FromMilliseconds(500);
+        _updateTimer.Tick += UpdateTimer_Tick;
+        _updateTimer.Start();
+    }
+
+    private void UpdateTimer_Tick(object sender, EventArgs e)
+    {
+        if (!_isDragging && Player.NaturalDuration.HasTimeSpan && _totalDuration.TotalSeconds > 0)
+        {
+            double current = Player.Position.TotalSeconds;
+            double total = _totalDuration.TotalSeconds;
+            ProgressSlider.Value = current / total;
+        }
+    }
+
+    private void StopUpdateTimer()
+    {
+        _updateTimer?.Stop();
+        _updateTimer = null;
+    }
+
+    private void VolumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+    {
+        Player.Volume = e.NewValue;
+    }
+
+    private void PlayTrack(AudioFileInfo trackInfo)
+    {
+        _currentTrack = trackInfo;
+
+        using var tagFile = TagLib.File.Create(trackInfo.FilePath);
+
+        #region TackInfoExtraction
+
+        if (tagFile.Tag.Pictures.Length != 0)
+        {
+            var picture = tagFile.Tag.Pictures[0];
+            var bitmapImageFromPicture = GetBitmapImageFromPicture(picture);
+            if (bitmapImageFromPicture != null)
+            {
+                TrackImage.Source = bitmapImageFromPicture;
+            }
+        }
+        else
+        {
+            TrackImage.Source = new BitmapImage(new Uri(TrackPlaceholderPath, UriKind.Relative));
+        }
+
+        TrackNameTextBlock.Text = trackInfo.Name;
+
+        TrackArtistTextBlock.Text = string.IsNullOrEmpty(tagFile.Tag.FirstPerformer)
+            ? "Неизвестен"
+            : tagFile.Tag.FirstPerformer;
+
+        TrackYearTextBlock.Text = tagFile.Tag.Year > 0
+            ? tagFile.Tag.Year.ToString()
+            : "Неизвестно";
+
+        TrackGenreTextBlock.Text = tagFile.Tag.Genres.Length > 0
+            ? string.Join(", ", tagFile.Tag.Genres)
+            : "Неизвестен";
+
+        TrackDistributionTextBlock.Text = string.IsNullOrEmpty(tagFile.Tag.Comment)
+            ? "Нет описания"
+            : tagFile.Tag.Comment;
+
+        #endregion
+
+        var selectedTrackUri = new Uri(trackInfo.FilePath, UriKind.Absolute);
+
+        Player.Stop();
+        StopUpdateTimer();
+        ProgressSlider.Value = 0;
+        _isPlaying = false;
+        PlayPauseImage.Source = new BitmapImage(new Uri("/Image/play.png", UriKind.Relative));
+
+        Player.MediaOpened -= OnMediaOpened;
+        Player.MediaOpened += OnMediaOpened;
+
+        Player.Close();
+        Player.Open(selectedTrackUri);
     }
 }
